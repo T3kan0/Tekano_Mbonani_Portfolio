ee9ddc8c5040dd03d1b63f371fd7820ed415d883
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-13 04:25:33 +0200

    Update 4_🔎_Projects.py

--- Portfolio/pages/4_🔎_Projects.py
+++ Portfolio/pages/4_🔎_Projects.py
@@ -24,27 +24,35 @@ st.set_page_config(
 # --- LOAD CSS, PDF & PROFIL PIC ---
 #with open('config.css') as f:
     #st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    .header-container {
+        background: linear-gradient(to right, #dbe9f4, #ffffff);
+        padding: 2rem;
+        border-radius: 10px;
+        box-shadow: 0px 2px 10px rgba(0,0,0,0.1);
+        margin-bottom: 1rem;
+    }
+    .header-title {
+        text-align: center;
+        color: #011f4b;
+        font-size: 3rem;
+        font-weight: bold;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+    }
+    </style>
+    <div class="header-container">
+        <div class="header-title">🗄️ Projects</div>
+    </div>
+""", unsafe_allow_html=True)
+
+
+
 
 # Set a fixed height for the iframes
 iframe_height = 600
 
-st.markdown("""
-    <style>
-    .center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100px;
-    }
-    </style>
-    <div class="center">
-        <h1 style="color: dimgrey;">Projects</h1>
-    </div>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-    <hr style="border: 2px solid dimgrey;">
-""", unsafe_allow_html=True)
 
 st.markdown("""
     <style>
