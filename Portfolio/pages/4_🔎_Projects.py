df51e7db1815bb7e7e0f8575f932dcb5652a2df4
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-15 20:45:47 +0200

    Update 4_🔎_Projects.py

--- Portfolio/pages/4_🔎_Projects.py
+++ Portfolio/pages/4_🔎_Projects.py
@@ -113,7 +113,7 @@ st.markdown("""
         justify-content: center;
         align-items: center;
         height: 100px;
-        background: linear-gradient(to right, #FAF9F6, #e4edf2);
+        background: linear-gradient(to right, #e4edf2, #FAF9F6);
         border-radius: 12px;
         box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
         margin-bottom: 20px;
