5f1463305c973d3891dba65c0c940966f049dfaa
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-15 20:44:00 +0200

    Update 4_🔎_Projects.py

--- Portfolio/pages/4_🔎_Projects.py
+++ Portfolio/pages/4_🔎_Projects.py
@@ -120,26 +120,7 @@ st.markdown("""
     }
     </style>
     <div class="center-header">
-        <h2 style="color: #011f4b; font-weight: 600;"></> Python</h2>
-    </div>
-""", unsafe_allow_html=True)
-
-
-st.markdown("""
-    <hr style="border: 2px solid dimgrey;">
-""", unsafe_allow_html=True)
-
-st.markdown("""
-    <style>
-    .center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100px;
-    }
-    </style>
-    <div class="center">
-        <h4 style="color: dimgrey;">Python</h4>
+        <h2 style="color: #011f4b; font-weight: 600;">🐍 Python</h2>
     </div>
 """, unsafe_allow_html=True)
 
