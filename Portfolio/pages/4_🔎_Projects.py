ae5d5c0e591df717253aa80df211e9a86ef01c6e
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-15 21:08:29 +0200

    Update 4_🔎_Projects.py

--- Portfolio/pages/4_🔎_Projects.py
+++ Portfolio/pages/4_🔎_Projects.py
@@ -289,8 +289,23 @@ with col9:
         """, unsafe_allow_html=True)
 
 st.markdown("""
-    <hr style="border: 2px solid dimgrey;">
+    <style>
+    .center-header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100px;
+        background: linear-gradient(to right, #FAF9F6, #e4edf2);
+        border-radius: 12px;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+        margin-bottom: 20px;
+    }
+    </style>
+    <div class="center-header">
+        <h2 style="color: #011f4b; font-weight: 600;">🤖 Websites</h2>
+    </div>
 """, unsafe_allow_html=True)
+
 
 st.markdown("""
     <style>
