c02bdfee5a24de37ffde5ae98590f565afd00108
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-15 21:04:20 +0200

    Update 4_🔎_Projects.py

--- Portfolio/pages/4_🔎_Projects.py
+++ Portfolio/pages/4_🔎_Projects.py
@@ -240,23 +240,10 @@ st.markdown("""
     }
     </style>
     <div class="center-header">
-        <h2 style="color: #011f4b; font-weight: 600;">🤖 Web Apps</h2>
+        <h2 style="color: #011f4b; font-weight: 600;">🤖 Web Applications</h2>
     </div>
 """, unsafe_allow_html=True)
 
-st.markdown("""
-    <style>
-    .center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100px;
-    }
-    </style>
-    <div class="center">
-        <h4 style="color: dimgrey;">Web Apps</h4>
-    </div>
-""", unsafe_allow_html=True)
 
 col8, col9 = st.columns(2)
 with col8:
