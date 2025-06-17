791232e5bcecd8d911d37b0df9f9edb6d88e2096
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-06-17 21:28:06 +0200

    Update 5_💬_Contact.py

--- Portfolio/pages/5_💬_Contact.py
+++ Portfolio/pages/5_💬_Contact.py
@@ -116,45 +116,6 @@ def contact_form():
 contact_form()
 
 
-
-
-#def contact_form():
-with st.form('contact_form'):
-    name = st.text_input('First Name')
-    email = st.text_input('Email Adress')
-    message = st.text_area('Your Message')
-    submit_button = st.form_submit_button('submit')
-
-    if submit_button:
-        if not WEBHOOK_URL:
-            st.error(
-                'Email service is not set up. Please try again later.'
-            )
-            st.stop()
-        if not name:
-            st.error('Please provide your name.', icon='👨🏼')
-            st.stop()
-        if not email:
-            st.error('Please provide your email address.', icon='📧')
-            st.stop()
-        if not is_valid_email(email):
-            st.error('Please provide a valid email address.', icon='📧')
-            st.stop()
-        
-        if not message:
-            st.error('Please provide a message.', icon='💬')
-            st.stop()
-        #prepare the data payload and send it to the specified webhook URL
-        data = {'email':email, 'name':name, 'message':message}
-        response = requests.post(WEBHOOK_URL, json=data)
-
-        if response.status_code == 200:
-            st.success('Your message has beenn sent successfully!', icon='💬')
-        else:
-            st.error('There was an error sending your message!', icon='😧')
-
-
-
 st.markdown("""
     <style>
     .closing-message {
