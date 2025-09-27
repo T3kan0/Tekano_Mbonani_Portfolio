281e1149fa2e473de177cff5faf46b9561bfe3fa
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-09-27 05:29:32 +0200

    Update 1_🏠_Home_Page.py

--- Portfolio/1_🏠_Home_Page.py
+++ Portfolio/1_🏠_Home_Page.py
@@ -33,6 +33,44 @@ response = requests.get(url)
 #else:
     #st.error("Failed to fetch CSS. Check the URL or connection.")
 
+st.markdown("""
+    <style>
+    .intro-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        background: linear-gradient(to right, #b3cde0, #ffffff);
+        border-radius: 12px;
+        box-shadow: 0 4px 12px rgba(0,0,0,0.05);
+        margin-bottom: 2rem;
+        border: 2px solid #011f4b;
+        
+        /* NEW: Sticky behavior */
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 2.875rem;  /* offset below Streamlit navbar */
+        z-index: 1000;  /* keep above other elements */
+    }
+    .intro-title {
+        font-size: 2.8rem;
+        font-weight: bold;
+        color: #011f4b;
+        margin-bottom: 0.5rem;
+    }
+    .intro-subtitle {
+        font-size: 1.3rem;
+        font-style: italic;
+        color: #7f8c8d;
+    }
+    </style>
+
+    <div class="intro-container">
+        <div class="intro-title">Tekano Mbonani, M.Sc.</div>
+        <div class="intro-subtitle">Welcome To My Portfolio</div>
+    </div>
+""", unsafe_allow_html=True)
 
 st.markdown("""
     <style>
